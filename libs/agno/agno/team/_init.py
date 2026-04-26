047e4bc4297dfbcb5e1ce1cc1f9ff8018fc1c371
mzm\mzm <151715612@qq.com>
2026-04-26 09:55:00 +0800

    feat: add override_member_history to Team for controlling member history loading

--- libs/agno/agno/team/_init.py
+++ libs/agno/agno/team/_init.py
@@ -128,6 +128,7 @@ def __init__(
     num_history_runs: Optional[int] = None,
     num_history_messages: Optional[int] = None,
     max_tool_calls_from_history: Optional[int] = None,
+    override_member_history: Optional[bool] = None,
     skills: Optional[Skills] = None,
     tools: Optional[Union[List[Union[Toolkit, Callable, Function, Dict]], Callable[..., List]]] = None,
     tool_call_limit: Optional[int] = None,
@@ -248,6 +249,7 @@ def __init__(
         team.num_history_runs = 3
 
     team.max_tool_calls_from_history = max_tool_calls_from_history
+    team.override_member_history = override_member_history
 
     team.add_team_history_to_members = add_team_history_to_members
     team.num_team_history_runs = num_team_history_runs
